65f4f7ca875da1944660b2c3fd83894a833d875c
m-akira0924 <mizushima.akira@i3design.co.jp>
2024-02-15 11:12:56 +0900

    index.tsxの構造化

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,60 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): React.JSX.Element => {
   return (
-    <View>
-      <Text>index.tsx</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Sample App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年1月1日 12:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年1月1日 12:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年1月1日 12:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
