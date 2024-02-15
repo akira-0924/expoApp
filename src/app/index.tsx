22eaf21aee3ac65c6a27e1ea9a3710b77f162456
m-akira0924 <mizushima.akira@i3design.co.jp>
2024-02-15 11:04:07 +0900

    expo router追加

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Index = (): React.JSX.Element => {
+  return (
+    <View>
+      <Text>index.tsx</Text>
+    </View>
+  )
+}
+
+export default Index
